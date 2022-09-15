3e296299c7bc6ba4c9f4e36dbe9511e82a0ca931
massouty <massouty@outlook.com>
2022-09-15 11:15:26 +0200

    Create main-view.jsx
    if applied , this commit will have created file main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className="main-view">
+        <div>Silence of the Lambs</div>
+        <div>Lord of the Ring</div>
+        <div>Coco</div>
+        <div>Braveheart</div>
+        <div>Hangover</div>
+        <div>Batman The Dark Knight</div>
+        <div>The Shawshank Redemption</div>
+        <div>The Hobbit</div>
+        <div>Forrest Gump</div>
+        <div>Midway</div>
+
+      </div>
+    );
+  }
+}
