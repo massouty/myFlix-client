5d1e353e3332c978b61e48f20d03db12c2434a79
massouty <massouty@outlook.com>
2022-09-19 14:09:18 +0200

    make reedited my project

--- myflix-client/src/components/movie-view/movie-view.jsx
+++ myflix-client/src/components/movie-view/movie-view.jsx
@@ -210,3 +210,5 @@ setSelectedMovie(newSelectedMovie) {
     );
   }
 }
+
+export default {MovieView}
--- myflix-client/src/index.js
+++ myflix-client/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import MainView from './components/main-view/main-view';
+import { MovieView } from './components/movie-view/movie-view';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Import statement to indicate that you need to bundle `./index.scss`
+import ('./index.scss');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// Main component (will eventually use all the others)
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+       <MainView />
+    );
+  }
+}
+
+// Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render your app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
